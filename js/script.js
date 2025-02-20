4bf253e525bc9c5b14a520e102e131d042bccee6
Danilo526 <dbonifacio167@gmail.com>
2025-02-20 18:27:02 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -97,3 +97,21 @@
 
 </body>
 </html>
+
+// Navegar para a tela de cadastro
+document.getElementById('cadastrar-btn').addEventListener('click', function() {
+    document.getElementById('login-screen').style.display = 'none';
+    document.getElementById('cadastro-screen').style.display = 'block';
+});
+
+// Voltar para a tela de login
+document.getElementById('voltar-btn').addEventListener('click', function() {
+    document.getElementById('cadastro-screen').style.display = 'none';
+    document.getElementById('login-screen').style.display = 'block';
+});
+
+// Implementar o logout
+document.getElementById('logout-btn').addEventListener('click', function() {
+    document.getElementById('main-screen').style.display = 'none';
+    document.getElementById('login-screen').style.display = 'block';
+});
